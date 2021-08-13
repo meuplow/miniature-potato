e82e005c6a911aa3d957ee6ab9ff2f48b18201bf
meuplow <gabrielgm@ufcspa.edu.br>
2021-08-13 17:08:26 -0300

    #1

---
+++ uri1002-springboot/src/main/java/com/example/springboot/Uri1002SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri1002SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri1002SpringbootApplication.class, args);
+	}
+
+}
---
+++ uri1002-springboot/src/test/java/com/example/springboot/Uri1002SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uri1002SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
